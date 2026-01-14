04657deda052d7349606b83540cf81bb658b36af
Bilal <bilalmohammed333999@gmail.com>
2026-01-14 22:26:26 +0530

    wrap children in dashboard provider eveyrwhere

--- app/Providers.tsx
+++ app/Providers.tsx
@@ -1,7 +1,12 @@
 "use client";
 
+import { DashboardProvider } from "@/components/Context/DashboardContext";
 import { SessionProvider } from "next-auth/react";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return <SessionProvider>
+    <DashboardProvider>
+    {children}
+    </DashboardProvider>
+    </SessionProvider>;
 }
