fbeff119f64bcda3c1510f42b6e9d55d5a6d2500
Bilal <bilalmohammed333999@gmail.com>
2026-01-27 19:09:45 +0530

    create empid table

--- lib/supabaseAdmin.ts
+++ lib/supabaseAdmin.ts
@@ -3,6 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 // This client is for SERVER-SIDE use only.
 // It uses the SERVICE_ROLE_KEY to bypass RLS because the 
 // Next.js API route handles the authentication logic.
+
 export const supabaseAdmin = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.SUPABASE_SERVICE_ROLE_KEY! // Securely stored in .env
